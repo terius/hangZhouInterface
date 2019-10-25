e9f742799e4474f9fb695a5577580ff7e6842d48
飞天猪 <teriusstar@hotmail.com>
2019-10-25 22:24:52 +0800

    aaaaa

---
+++ HangZhouService/Install.bat
@@ -0,0 +1,5 @@
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\installutil.exe /u GZLBService.exe
+
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\installutil.exe GZLBService.exe
+Net Start GZLBInterfaceService
+pause
